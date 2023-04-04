f1ebe044da417d12ebeae027f99d9341912597cc
Guilherme Garcia <garciaguig@gmail.com>
2023-04-04 20:36:29 -0300

    feat: Add json file reading function to 'import_data' (class Invetory)

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,9 +1,19 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
+    def __read_file(path: str, file):
+        file_extension = path.split(".")[-1]
+
+        if file_extension == "csv":
+            return csv.DictReader(file, delimiter=",", quotechar='"')
+
+        elif file_extension == "json":
+            return json.load(file)
+
     @classmethod
     def import_data(cls, path: str, report_type: str):
         report = SimpleReport if report_type == "simples" else CompleteReport
@@ -12,7 +22,7 @@ class Inventory:
             invetory = []
 
             with open(path) as file:
-                data = csv.DictReader(file, delimiter=",", quotechar='"')
+                data = cls.__read_file(path, file)
 
                 for row in data:
                     invetory.append(row)
@@ -21,3 +31,6 @@ class Inventory:
 
         except FileNotFoundError:
             print("File not found, check if the path is valid")
+
+
+print(Inventory.import_data("inventory_report/data/inventory.json", "simples"))
