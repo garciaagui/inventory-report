a133351f2a6051f44ccb650243b2ac66b6ddd749
Guilherme Garcia <garciaguig@gmail.com>
2023-04-05 12:27:42 -0300

    test: Add teste for Product print

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
-def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+def test_relatorio_produto(capsys):
+    new_product = Product(
+        11,
+        "Mark III",
+        "Stark Industries",
+        "2023-04-04",
+        "2025-04-04",
+        "FR29 5791 5333 58XR G4PR IG28 D08",
+        "longe do Thanos",
+    )
+
+    expected_print_output = (
+        f"O produto {new_product.nome_do_produto}"
+        f" fabricado em {new_product.data_de_fabricacao}"
+        f" por {new_product.nome_da_empresa} com validade"
+        f" até {new_product.data_de_validade}"
+        f" precisa ser armazenado {new_product.instrucoes_de_armazenamento}.\n"
+    )
+
+    print(new_product)
+    captured, _ = capsys.readouterr()
+
+    assert expected_print_output == captured
