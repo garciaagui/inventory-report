c101ea4ccf33b24b9e4176624af9a54702bf5e21
Guilherme Garcia <garciaguig@gmail.com>
2023-04-10 20:13:52 -0300

    refactor: Refactor Inventory class

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,71 +1,20 @@
+from collections.abc import Iterable
+from inventory_report.inventory.inventory_iterator import InventoryIterator
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-import csv
-import json
-import xml.etree.ElementTree as ET
 
 
-class Inventory:
-    def __read_csv(file, inventory):
-        data = csv.DictReader(file, delimiter=",", quotechar='"')
+class Inventory(Iterable):
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
 
-        for row in data:
-            inventory.append(row)
-
-        return inventory
-
-    def __read_json(file, inventory):
-        data = json.load(file)
-
-        for row in data:
-            inventory.append(row)
-
-        return inventory
-
-    def __read_xml(file, inventory):
-        tree = ET.parse(file)
-        root = tree.getroot()
-
-        for record in root.findall("record"):
-            product = dict(
-                id=record.find("id").text,
-                nome_do_produto=record.find("nome_do_produto").text,
-                nome_da_empresa=record.find("nome_da_empresa").text,
-                data_de_fabricacao=record.find("data_de_fabricacao").text,
-                data_de_validade=record.find("data_de_validade").text,
-                numero_de_serie=record.find("numero_de_serie").text,
-                instrucoes_de_armazenamento=record.find(
-                    "instrucoes_de_armazenamento"
-                ).text,
-            )
-
-            inventory.append(product)
-
-        return inventory
-
-    @classmethod
-    def import_data(cls, path: str, report_type: str):
+    def import_data(self, path: str, report_type: str):
         report = SimpleReport if report_type == "simples" else CompleteReport
+        imported_data = self.importer.import_data(path)
+        self.data.extend(imported_data)
 
-        try:
-            inventory = []
+        return report.generate(self.data)
 
-            with open(path) as file:
-                file_extension = path.split(".")[-1]
-
-                if file_extension == "csv":
-                    inventory = cls.__read_csv(file, inventory)
-
-                elif file_extension == "json":
-                    inventory = cls.__read_json(file, inventory)
-
-                else:
-                    inventory = cls.__read_xml(file, inventory)
-
-            return report.generate(inventory)
-
-        except FileNotFoundError:
-            print("File not found, check if the path is valid")
-
-
-#
+    def __iter__(self):
+        return InventoryIterator(self.data)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,5 +1,5 @@
 import sys
-from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.inventory.inventory_refactor import Inventory
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
@@ -7,13 +7,13 @@ from inventory_report.importer.xml_importer import XmlImporter
 
 def generate_report(path: str, report_type: str):
     if path.endswith(".csv"):
-        return InventoryRefactor(CsvImporter).import_data(path, report_type)
+        return Inventory(CsvImporter).import_data(path, report_type)
 
     elif path.endswith(".json"):
-        return InventoryRefactor(JsonImporter).import_data(path, report_type)
+        return Inventory(JsonImporter).import_data(path, report_type)
 
     elif path.endswith(".xml"):
-        return InventoryRefactor(XmlImporter).import_data(path, report_type)
+        return Inventory(XmlImporter).import_data(path, report_type)
 
     else:
         raise ValueError("Arquivo inválido")
